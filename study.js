b4f2d575485a1ad6b2591d5b69ee7a84301efec3
aeroavi09 <154035593+aeroavi09@users.noreply.github.com>
2024-05-23 22:56:09 -0500

    Update study.js
    'Bearer '+ $API_KEY

--- study.js
+++ study.js
@@ -26,7 +26,7 @@ async function question() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': $os.getenv("TEST_1") // Replace with your OpenAI API key
+          'Authorization': 'Bearer '+ $API_KEY // Replace with your OpenAI API key
         },
         body: JSON.stringify({
           temperature: 0.7,
@@ -66,7 +66,7 @@ async function question() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': '$os.getenv("TEST_1")' // Replace with your OpenAI API key
+          'Authorization': 'Bearer '+ $API_KEY // Replace with your OpenAI API key
         },
         body: JSON.stringify({
           temperature: 0.7,
